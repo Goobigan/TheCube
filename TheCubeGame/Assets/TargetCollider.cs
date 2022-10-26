797694329c57b1404de2f5985e4621474b9d992e
STUDENT Dj Mc Carthy <Dj.McCarthy@students.ittralee.ie>
2022-10-26 10:50:38 +0100

    Update 9
    Targets now get hit, and the counter is functional, some glitches to be fixed, camera to be fixed
    Signed-off-by: STUDENT Dj Mc Carthy <Dj.McCarthy@students.ittralee.ie>

--- TheCubeGame/Assets/TargetCollider.cs
+++ TheCubeGame/Assets/TargetCollider.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class TargetCollider : MonoBehaviour
 
 {
-    private int NumberOfTargets;
+    public int NumberOfTargets;
     public GameObject target;
+    int TargetCH;
+    public int TargetMaxHealth=150;
+
     // Start is called before the first frame update
     void Start()
     {
         NumberOfTargets = 10;
+        TargetCH = TargetMaxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        private void OnCollisionEnter(Collision collision)
+       
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        if (NumberOfTargets == 1)
         {
-            
-            if (NumberOfTargets = 1)
-            {
-                print("Mission Complete!");
-            }
-            else
-            {
-                print("One Target Down! Only " + (NumberOfTargets - 1) + "Remaining");
-            }
-            Destroy(Target);
+            print("Mission Complete!");
         }
+        else
+        {
+            print("One Target Down! Only " + (NumberOfTargets - 1) + "Remaining");
+            NumberOfTargets = NumberOfTargets - 1;
+        }
+        Destroy(obj: target, 0);
     }
 }
